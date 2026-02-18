82425a37b06e3cc4f53612568b8fc41cc6fe1d42
Dalton Pinto <dalton.pinto@cloudwalk.io>
2026-02-18 10:25:19 +0100

    use tokyonight theme

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = {
+      style = "night",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
